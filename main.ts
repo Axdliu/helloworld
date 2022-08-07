3b45e002238a1c39d147c60a5b0e40aa9cd99f1a
Arnold <axdliu@gmail.com>
2022-08-06 22:08:42 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     counter += 1
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
     basic.showNumber(counter)
 })
 input.onButtonPressed(Button.B, function () {
     counter += -1
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
     basic.showNumber(counter)
 })
 let counter = 0
@@ -11,5 +13,5 @@ basic.showString("Counter")
 counter = 0
 basic.showNumber(counter)
 basic.forever(function () {
-    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+	
 })
